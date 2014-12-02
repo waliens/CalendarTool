4d041a0fe6eaba958c1a8caf49cd1ab7aea58d8f
L <laurent.vanosmael@student.ulg.ac.be>
2014-12-02 16:25:14 +0100

    Init branch

--- web/class/Event.class.php
+++ web/class/Event.class.php
@@ -1,9 +1,16 @@
 <?php
 
-class Event{
+//Maybe this class should be abstract
+class Event extends Model{
 	protected $_name, 
 			  $_description,
 			  $_id,
 			  $_place,
 			  $_category; // @type : EventCategory
+	
+	
+
 }
+
+
+?>
