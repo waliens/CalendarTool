3c6c02021bf686b5e400c49464f8db94c62dbf9d
Waliens <romainmormont@hotmail.com>
2014-12-08 23:12:46 +0100

    change root folder

---
+++ web/ct/util/entry_point/Browser.class.php
@@ -0,0 +1,23 @@
+<?php
+
+    /**
+     * @file 
+     * @brief Browser entry point
+     */
+
+    namespace util\entry_point;
+    
+    /**
+     * @class Browser
+     * @brief This class must be implemented by any request handler
+     */
+    class Browser implements EntryPoint
+    {
+        /**
+         * @copydoc EntryPoint::get_controller
+         */
+        public function get_controller()
+        {
+
+        }
+    };
