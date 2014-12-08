27a10325db61ad8f0ed45e68a92c3bb945634b51
Waliens <romainmormont@hotmail.com>
2014-12-08 23:13:03 +0100

    add mvc base classes

---
+++ web/ct/util/mvc/Controller.class.php
@@ -0,0 +1,17 @@
+<?php
+
+	/**
+	 * @file
+	 * @brief Controller class
+	 */
+
+	namespace ct\util\mvc;
+
+	/**
+	 * @class Controller 
+	 * @brief Superclass of any controller class
+	 */
+	abstract class Controller
+	{
+		
+	}
