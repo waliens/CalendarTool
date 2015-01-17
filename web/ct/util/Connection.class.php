3a930813a1d66458f3e3e1ee59200456c44d6079
Waliens <romainmormont@hotmail.com>
2015-01-17 18:12:36 +0100

    Update Connection class to take into account the reverso-proxy and root mode (impl. not finished)

--- web/ct/util/Connection.class.php
+++ web/ct/util/Connection.class.php
@@ -7,123 +7,124 @@
 
 	namespace ct\util;
 
-	use model\LoginModel;
+	use ct\util\superglobals\Superglobal as Superglobal;
+	use ct\util\superglobals\SG_Session as SG_Session;
+
+	use ct\util\Redirection as Redirection;
 
 	/**
 	 * @class Connection
-	 * @brief A class for handling connection to the website
+	 * @brief A class for handling an user connection and accessing information about the currently connected user
+	 * @todo End the implementation of the constructor (needs a model)
 	 */
 	class Connection
 	{
-		private $is_connected = false;
-		private $login;
-		private $pass;
-		private $user; 
+		private $sess; /**< @brief : a SG_Session object */
 
 		/**
 		 * @brief Construct a Connection objects
 		 */
 		public function __construct()
 		{
-			if(!$this->session_ok())
-				session_start();
-			/*
-			$log_mod = new LoginModel();
-			
-			if(isset($_SESSION['user'], $_SESSION['login'], $_SESSION['pass']) 
-					&& $log_mod->checkPassword($_SESSION['login'], $_SESSION['pass']))
-			{
-				// check connexion data in the database
-				$this->login = $_SESSION['login'];
-				$this->pass = $_SESSION['pass'];
-				$this->user = $_SESSION['user'];
-				$this->is_connected = true;
-			}
-			else
-				$this->is_connected = false;
-			*/
-		}
+			$this->sess = new SG_Session();
 
-		public function get_user()
-		{
-			if(!$this->isConnected())
-				throw new Exception("User not connected");
-		
-			return $this->login;
+			$ulg_id = $this->extract_ulg_id_from_http();
+			
+			if(!$this->is_connected()) // no previous connection
+				$this->connect($ulg_id);
+			else if($this->user_id() !== $ulg_id) // previous ulg id doesn't match the current
+				$this->disconnect();
+			else
+			{
+				$this->connect($ulg_id);
+
+				if(/* attemp to connect to root and valid ids*/)
+					$this->connect_root(/* root id */);
+				elseif(/* invalid password */)
+					$this->disconnect_root();
+			}
 		}
 
 		/**
-		 * @brief Check whether the user is connected
-		 * @retval bool True if the user is connected, false otherwise
+		 * @brief Extract the connected user's id from the HTTP request header 
+		 * @retval string The ulg id of the connected user, an empty string if there is none
+		 * @todo specify the format of the ulg id returned by the function
+		 */
+		private function extract_ulg_id_from_http()
+		{
+			$http_headers = array();
+
+			foreach (getallheaders() as $key => $value) 
+				$http_headers[strtolower($key)] = $value;
+
+			return isset($http_headers['x-remote-user']) ? $http_headers['x-remote-user'] : "";
+ 		}
+
+		/**
+		 * Checks whether an user is already connected for the given session
+		 * @retval bool True if an user is connected for the current session, false otherwise
 		 */
 		public function is_connected()
 		{
-			return $this->is_connected;
+			return $this->sess->check("ulg_id") == Superglobal::ERR_OK;
 		}
 
 		/**
-		 * @brief Check whether the session is started
-		 * @retval bool True if the session was started, false otherwise
+		 * @brief Check whether the user is connected as root
+		 * @retval bool True if the user is connected as root, false otherwise
 		 */
-		private function session_ok()
+		public function is_root()
 		{
-			return session_id() !== "";
+			return $this->sess->check("root_id");
+		}
+
+ 		/**
+ 		 * @brief Return the ulg id of the currently connected user
+ 		 * @retval string the ulg id 
+ 		 */
+		public function user_id()
+		{
+			if(!$this->is_connected())
+				throw new Exception("User not connected");
+		
+			return $this->sess->value("ulg_id");
 		}
 
 		/**
 		 * @brief Connects the user
-		 * @param[in] string $login The login of the user to connect
-		 * @param[in] string $pass ????
-		 * @retval bool True if the connection has succeeded, false otherwise
+		 * @param[in] string $ulg_id The login of the user to connect
 		 */
-		public function connect($login, $pass)
+		private function connect($ulg_id)
 		{
-			$log_mod = new LoginModel();
-
-			if($log_mod->checkPassword($login, $pass))
-			{
-				if(!$this->session_ok())
-					session_start();
-
-				$user = $log_mod->getUserByLogin($login);
-
-				$_SESSION['login'] = $login;
-				$_SESSION['pass'] = $pass;
-				$_SESSION['user'] = $user['Id_User'];
-
-				$this->login = $login;
-				$this->pass = $pass;
-				$this->user = $user['Id_User'];
-				$this->is_connected = true;
-
-				return true;
-			}
-
-			return false;
+			$_SESSION['ulg_id'] = $ulg_id;
 		}
 
 		/**
-		 * @brief Disconnect the current user
-		 * @retval bool True if the user was successfully disconnected, false otherwise
+		 * @brief Connects the user as root
+		 * @param[in] string $root_id The id of the root user
+		 */
+		private function connect_root($root_id)
+		{
+			$_SESSION['root_id'] = $root_id;
+		}
+
+		/**
+		 * @brief Disconnect the current user and redirect him to the ULg logout page
 		 */
 		public function disconnect()
 		{
-			if(!$this->session_ok())
-				session_start();
-
 			$_SESSION = array();
 
 			session_destroy();
 
-			return true;
+			new Redirection("http://www.intranet.ulg.ac.be/logout");
 		}
 
 		/**
-		 * @brief Return information about the currently connected user
-		 * @retval array The user information
+		 * @brief Disconnect the current user's root connection
 		 */
-		public function userConnected()
+		public function disconnect_root()
 		{
-
+			unset($_SESSION['root_id']);
 		}
 	}
