6c5e8a1767c937a6408811b474b8edcf68c3ea96
charybdeBE <momiamoldava@gmail.com>
2015-02-28 18:07:41 +0100

    The PrivateEventController is now connected via the getController method of ajax

--- web/util/entry_point/Ajax.class.php
+++ web/util/entry_point/Ajax.class.php
@@ -11,13 +11,32 @@
      * @class Ajax
      * @brief This class must be implemented by any request handler
      */
-    class Ajax implements EntryPoint
+    use ct\controllers\ajax\PrivateEventController;
+
+				use util\superglobals\Superglobal;
+
+				class Ajax implements EntryPoint
     {
+    	protected $sg_get; /**< @brief Superglobal object for $_GET */
         /**
          * @copydoc EntryPoint::get_controller
          */
-        public function get_controller()
+        public function __construct(){
+        	$this->sg_get = new SG_Get();
+        	 
+        }
+    	public function get_controller()
         {
-
+			if($this->sg_get->check("req") == Superglobal::ERR_OK){
+				$req = $this->sg_get->value("req");
+				switch($req){
+					case 101:
+						return new PrivateEventController();
+						break;
+					default:
+						return null;
+						break;
+				}
+			}
         }
     };
