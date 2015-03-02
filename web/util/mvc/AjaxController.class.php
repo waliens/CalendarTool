2cb9f4ec6e4c0a50be29f2224af933b3cc8e82de
Waliens <romainmormont@hotmail.com>
2015-03-02 21:52:52 +0100

    add predefined error in ajax controller

--- web/util/mvc/AjaxController.class.php
+++ web/util/mvc/AjaxController.class.php
@@ -18,6 +18,11 @@
 		
 		/* 000 : generic error */
 		const ERROR = 0;
+		/* 3xx : missing data */
+		const ERROR_MISSING_DATA = 300; /**< @brief Missing data generic error (300) */
+		const ERROR_MISSING_USER = 301; /**< @brief User is missing */
+		const ERROR_MISSING_EVENT = 302; /**< @brief Event is missing */
+		const ERROR_MISSING_GLOBAL_EVENT = 303; /**< @brief Missing global event */
 
 		/* 4xx : access error (user has not the given rights) */
 		const ERROR_ACCESS_DENIED = 400; /**< @brief Access denied generic error (400) */
@@ -48,9 +53,26 @@
 			// $this->error_msgs[] = array("EN" => "", "FR" => "");
 
 			/* 000 : no error */
-			$this->error_msgs[self::ERROR] = 
-				= array("EN" => "An error occurred", 
+			$this->error_msgs[self::ERROR]
+				= array("EN" => "An error occurred.", 
 						"FR" => "Une erreur s'est produite.");
+
+			/* 300 : missing */
+			$this->error_msgs[self::ERROR_MISSING_DATA] 
+				= array("EN" => "Missing data : the data you're looking for was not found.", 
+						 "FR" => "Données manquantes : les données recherchées sont introuvables.");
+
+			$this->error_msgs[self::ERROR_MISSING_USER] 
+				= array("EN" => "User not found : the user you're looking for was not found.", 
+						"FR" => "Utilisateur manquant : l'utilisateur recherché n'a pas été trouvé.");
+
+			$this->error_msgs[self::ERROR_MISSING_EVENT] 
+				= array("EN" => "Event not found : the event you were looking for wasn't found", 
+						"FR" => "Evénement manquant : l'événement recherché n'a pas été trouvé.");
+
+			$this->error_msgs[self::ERROR_MISSING_GLOBAL_EVENT] 
+				= array("EN" => "Global event not found : the global event you're looking for wasn't found.", 
+						"FR" => "Evénement global manquant : l'événement global recherché n'a pas été trouvé.");
 
 			/* 400 : access denied */
 			$this->error_msgs[self::ERROR_ACCESS_DENIED] 
