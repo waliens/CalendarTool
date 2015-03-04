3af344d3af02c17df19181d23bdac8a1c33d3d75
charybdeBE <momiamoldava@gmail.com>
2015-03-04 12:04:05 +0100

    GetPE controller

---
+++ web/ct/controllers/ajax/GetPrivateEvent.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file
+ * @brief Private Event ControllerClass
+ */
+
+namespace ct\controllers\ajax;
+
+
+use ct\models\events\StudentEventModel;
+use util\mvc\AjaxController;
+use util\superglobals\Superglobal;
+
+/**
+ * @class Event
+ * @brief Class for handling the control of event
+ */
+
+class PrivateEventController extends AjaxController
+{
+
+		
+	public function __construct() 
+	{
+		parent::__construct();
+
+
+		// create private event
+		$model = new StudentEventModel();
+		{events:[{
+			id, name, recurrence}]}
+
+		// get owner id
+		$id = $this->connection->user_id();
+		
+		$ret = array(); 
+		
+		$ret_sql = $model->getEvent(array("id_owner" => $id), array("id_event", "name", "id_recurrence"));
+		
+		foreach($ret as $key => $value){
+			array_push($ret, array("name" => $value["Name"], "id" => $value['Id_Event'], "recurrence" => $value['Id_Recurrence']));
+		}
+
+		$this->output_data['event'] = $ret;
+		$this->set_error($model->get_error());
+		
+	}
+	
+
+}
+
+
