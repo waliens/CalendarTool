6e92c50a7c569465b7ea60bb420191c6fd7e8d78
Waliens <romainmormont@hotmail.com>
2015-03-08 19:30:02 +0100

    add implementation of the ICSModel class

--- web/ct/models/ICSModel.class.php
+++ web/ct/models/ICSModel.class.php
@@ -9,6 +9,8 @@
 
 	use util\mvc\Model;
 
+	use ct\models\FilterCollectionModel;
+
 	/**
 	 * @class ICSModel
 	 * @brief A class for generating ICS calendars for a set of events
@@ -16,12 +18,15 @@
 	class ICSModel extends Model
 	{
 		private $smarty; /**< @brief The smarty object for generating ICS */
-		private $events; /**< @brief Array containing the events */
+		private $filter_collection; /**< @brief A filter collection object */
+
+		const PROD_ID = "ULg//Montefiore//PROJ0010//Group2//CalendarTool"; /**< @brief ICS Calendar product id */
 
 		/**
 		 * @brief Construct an ICSModel object
+		 * @param[in] FilterCollection A filter collection object
 		 */
-		public function __construct()
+		public function __construct(FilterCollection $filter_collection)
 		{
 			parent::__construct();
 
@@ -29,44 +34,79 @@
 			$this->smarty->setTemplateDir("views/tpl/export");
 			$this->smarty->setCompileDir("views/compiled/export");
 
-			$this->events = array();
-		}
-
-		/**
-		 * @brief Add some events to add into the final ics file
-		 * @param[in] array $events An array of integers being event ids
-		 */
-		public function add_events(array $events)
-		{
-			$this->events = array_unique(array_merge($this->events, $events));
+			$this->filter_collection = $filter_collection;
 		}
 
 		/**
 		 * @brief Generate a ICS calendar string from the stored events
-		 * @retval string The ICS content
+		 * @retval string The ICS string
 		 */
 		public function get_ics()
 		{
-			$events = array(); // get events;
+			$events = $this->filter_collection->get_events();
+			$formatted_events = array(); 
 
-			foreach(array() as $event)
+			// format the event's data for the ics file
+			foreach($events as &$event)
 			{
-				$ics_event = ;
+				$ics_event = array();
+				
+				$ics_event['uid'] = $event['Id_Event']."@calendartool.ulg.ac.be";
+				$ics_event['dtstamp'] = self::get_ics_date(date("Y-m-d H:i:s"));
+				$ics_event['dtstart'] = self::get_ics_date($event['Start']);
+
+				if(!empty($event['End']))
+					$ics_event['dtend'] = self::get_ics_date($event['End']);
+
+				$ics_event['location'] = self::get_ics_txt($event['Place']);
+				$ics_event['description'] = self::get_ics_txt($event['Description']);
+				$ics_event['categories'] = self::get_ics_txt($event['Categ_Name_FR']).", ".
+											self::get_ics_txt($event['Categ_Name_EN']);
+
+				$formatted_events[] = $ics_event;
 			}
+
+			$this->smarty->assign("events", $formatted_events);
+			$this->smarty->assign("prod_id", self::PROD_ID);
+
+			return $this->smarty->fetch("ics.tpl");
 		}
 
+
 		/**
-		 * @brief Return the date 
-		 * @param[in] string $date The date in french format
-		 * @retval string The ics date (YYYYMMDD "T" HHMMSS)
+		 * @brief Return the given date in the ICS format (YYYYMMDD "T" HHMMSS)
+		 * @param[in] string $date The date in SQL datetime/date format
+		 * @retval string The date in the ics format 
 		 */
-		private function get_ics_date($date)
+		private static function get_ics_date($date)
 		{
 			$matches = array();
 
-			if(!preg_match("#^([0-9]{2})[/-]([0-9]{2})[/-]([0-9]{4}) ([0-9]{2}):([0-9]{2}):([0-9]{2})$#", $date, $matches))
+			// workaround for transforming any date/datetime string into a datetime one
+			$date = date("Y-m-d H:i:s", strtotime($date));
+
+			if(!preg_match("#^([0-9]{4})[/-]([0-9]{2})[/-]([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})$#", $date, $matches))
 				return false;
 
 			return $matches[3].$matches[2].$matches[1]."T".$matches[4].$matches[5].$matches[6];
+		}
+
+		/**
+		 * @brief Format a text string so that it can be used in an ICS file
+		 * @param[in] string $text The text to process
+		 * @retval string The processed text string 
+		 */
+		private static function get_ics_txt($text)
+		{
+			/**
+			 * The following char must be escaped : '\', ';', ',' 
+			 * The newline character should be represented as linefeed '\n'
+			 */
+			$rep_map = array("\n?\r" => "\n", 
+							  ";"    => "\\;", 
+							  ","    => "\\,",
+							  "\\"   => "\\\\");
+
+			return preg_replace(array_keys($rep_map), array_values($rep_map), $text);
 		}
 	}
