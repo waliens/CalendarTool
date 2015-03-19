2768dee1641d72d58410a2c65a15dab45989f5b3
Waliens <romainmormont@hotmail.com>
2015-03-19 23:15:43 +0100

    change name of the get global event by student to by user (to allow prof user to use it)

---
+++ web/ct/controllers/ajax/GetGlobalEventsByUserController.class.php
@@ -0,0 +1,47 @@
+<?php
+
+	/**
+	 * @file 
+	 * @brief Contains the GetGlobalEventsByUserController class
+	 */
+
+	namespace ct\controllers\ajax;
+
+	use util\mvc\AjaxController;
+	use ct\models\events\GlobalEventModel;
+
+	/**
+	 * @class GetGlobalEventsByUserController
+	 * @brief A class for handling the get global event by user controller 
+	 */
+	class GetGlobalEventsByUserController extends AjaxController
+	{
+		/**
+		 * @brief Constrcut the GetGlobalEventsByUserController and process the request
+		 */ 
+		public function __construct()
+		{
+			parent::__construct();
+
+			// instantiate models 
+			$glob_mod = new GlobalEventModel();
+
+			// check user access
+			if($this->connection->user_is_student())
+			{	
+				$by = GlobalEventModel::GET_BY_STUDENT;
+				$id = $this->connection->user_id();
+			}
+			else
+			{
+				$by = GlobalEventModel::GET_BY_ACAD_YEAR;
+				$id = \ct\get_academic_year();
+			}
+
+			// fetch user's global events
+			$global_events = $glob_mod->get_global_events($by, $id);
+
+			$trans_glob = array("id" => "", "ulg_id" => "code", "name_long" => "name");
+			$this->add_output_data("courses", \ct\darray_transform($global_events, $trans_glob));
+		}
+	}
