0ce7ea0b4bf2d0256007452c659e1cf61ac2a763
michele.imperiali <michele.imperiali@hotmail.it>
2015-03-24 00:19:17 +0100

    Small correction bug#3

--- web/js/export.js
+++ web/js/export.js
@@ -100,9 +100,9 @@ $('#filter_alert').on('show.bs.modal', function (event) {
 							var cell1=row.insertCell(0);
 							var cell2=row.insertCell(1);
 							var cell3=row.insertCell(2);
-							cell1.innerHTML="ID";
+							cell1.innerHTML="Code";
 							cell1.className="min-width-110"
-							cell2.innerHTML="Title";
+							cell2.innerHTML="Titre";
 							cell3.innerHTML="Choisir";
 							filter_alert.append(table);
 							for (var i = 0; i < courses.length; i++)
