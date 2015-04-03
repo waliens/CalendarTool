cdbeeb42c40dd419acc572516cca8d49af013618
Waliens <romainmormont@hotmail.com>
2015-04-03 12:13:00 +0200

    solve issue #92

--- web/ct/Connection.class.php
+++ web/ct/Connection.class.php
@@ -60,8 +60,8 @@
 
 		//	$this->remote_user = "s060934"; // 3e BAC 
 		//	$this->remote_user = "s114352"; // 2e BAC
-			$this->remote_user = "s023178";
-		//	$this->remote_user = "u013317"; // Pascal Gribaumont
+		//	$this->remote_user = "s023178";
+			$this->remote_user = "u013317"; // Pascal Gribaumont
 			$this->host = "";
 			$this->connect($this->remote_user);
 			// check host
